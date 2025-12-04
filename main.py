fd71285cfe9c4bbda36fdb5b2574070ef0ee6bdc
ziyanaaan <ktziyanamnu@gmail.com>
2025-12-04 07:10:25 +0000

    main updated to stage1

--- main.py
+++ main.py
@@ -3,30 +3,10 @@ from models import Product
 
 app= FastAPI()
 
-@app.get("/products")
-def products():
-	return products 
-	
-
-products=[
-	Product(id=1,name="phone",quantity=20,price =5000),
-	Product(id=2,name="laptop",quantity=40,price=6000)
-]
-
-
-@app.get("/products/{id}")
-def getproducts(id: int):
-	for Product in products:
-		if Product.id==id:
-			return Product
-	return "Not found in Base"
-		
-	
 @app.get("/")
-def greet():
-	return "hello this is server"
+def runstat():
+    return "App is running"
 
-@app.post("/products")
-def postproducts(product: Product):
-	products.append(product)
-	return product
+@app.get("/health")
+def healthstat():
+    return "Status : Healthy"
