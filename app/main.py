0b377e3495eed23c20aff8d0ae2427589fbe63a8
ziyanaaan <ktziyanamnu@gmail.com>
2025-12-08 18:26:52 +0000

    Added server load and time endpoints, updated requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from models import Product
+import psutil
+from datetime import datetime
 
 app= FastAPI()
 
@@ -10,3 +11,26 @@ def runstat():
 @app.get("/health")
 def healthstat():
     return "Status : Healthy"
+
+@app.get("/status/server")
+def serverHealth():
+    return "Server is healthy"
+
+@app.get("/status/cpu")
+def getCpuStat():
+    cpu_use = psutil.cpu_percent(interval=1)
+    return { 
+        "Status": "Running",
+            "CPU usage" : cpu_use
+        }
+
+@app.get("/current-time")
+def get_current_time():
+    now = datetime.now()
+    
+    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return {
+        "status": "success",
+        "timestamp": formatted_time
+    }
